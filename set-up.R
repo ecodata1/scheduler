a90d610108d0d708e44c28c11e53c255ad4205bf
Foster <Tim.Foster@SEPA.org.uk>
2022-12-14 17:11:18 +0000

    tweak

--- set-up.R
+++ set-up.R
@@ -1,11 +1,14 @@
 
-install.packages("taskscheduleR",
+install.packages(c("taskscheduleR","rappdirs"),
   repos = "https://cloud.r-project.org"
 )
 library(taskscheduleR)
+library(rappdirs)
+dir <- user_cache_dir()
+path <- paste0(dir, "/install-packages.R")
 download.file(
   "https://raw.githubusercontent.com/ecodata1/scheduler/main/install-packages.R",
-  "C:/Program Files/R/test/install-packages.R"
+  path
 )
 source("install-packages.R")
 rscript <- paste0(here::here(), "/set-up.R")
