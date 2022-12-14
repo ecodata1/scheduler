8c179fbf163d6d1bf55254510cfabe93f10a752b
Foster <Tim.Foster@SEPA.org.uk>
2022-12-14 16:54:40 +0000

    tweak

--- set-up.R
+++ set-up.R
@@ -1,14 +1,19 @@
 
 install.packages("taskscheduleR",
-                 repos = "https://cloud.r-project.org")
+  repos = "https://cloud.r-project.org"
+)
 library(taskscheduleR)
-download.file("https://raw.githubusercontent.com/ecodata1/scheduler/main/install-packages.R",
-              "install-packages.R")
+download.file(
+  "https://raw.githubusercontent.com/ecodata1/scheduler/main/install-packages.R",
+  "C:/Program Files/R/test/install-packages.R"
+)
 source("install-packages.R")
 rscript <- paste0(here::here(), "/set-up.R")
 taskscheduler_delete(taskname = taskname)
-taskscheduler_create(taskname = taskname,
-                     rscript = rscript,
-                     schedule = schedule,
-                     starttime = starttime, )
+taskscheduler_create(
+  taskname = taskname,
+  rscript = rscript,
+  schedule = schedule,
+  starttime = starttime,
+)
 message("R packages install complete")
