c89c724df1807a344c63faca2857f0c35be7d623
Foster <Tim.Foster@SEPA.org.uk>
2022-12-14 15:39:51 +0000

    tweaks

--- install-packages.R
+++ install-packages.R
@@ -1,27 +1,34 @@
 # Edit list of packages as required
 # Install CRAN packages --------------------------------------------------------
-install.packages(c("devtools",
-                   "here",
-                   "tidyverse",
-                   "readxl",
-                   "openxlsx",
-                   "shiny",
-                   "shinyjs",
-                   "shinydashboard",
-                   "stringr",
-                   "mgcv",
-                   "knitr",
-                   "rmarkdown"), repos = "https://cloud.r-project.org")
+install.packages(c(
+  "devtools",
+  "here",
+  "cli",
+  "tidyverse",
+  "readxl",
+  "openxlsx",
+  "shiny",
+  "shinyjs",
+  "shinydashboard",
+  "stringr",
+  "mgcv",
+  "knitr",
+  "rmarkdown"
+),
+repos = "https://cloud.r-project.org"
+)
 
 # Install github packages ------------------------------------------------------
 library("devtools")
 devtools::install_github("ecodata1/darleq3",
-                         ref = "description",
-                         dependencies = TRUE,
-                         upgrade = TRUE)
+  ref = "description",
+  dependencies = TRUE,
+  upgrade = TRUE
+)
 devtools::install_github("ecodata1/hera",
-                         dependencies = TRUE,
-                         upgrade = TRUE)
+  dependencies = TRUE,
+  upgrade = TRUE
+)
 
 # Params for taskscheduler_create() function -----------------------------------
 # Create globals for set-up.R script
@@ -29,5 +36,3 @@ taskname <<- "install_r_packages"
 rscript <<- paste0(here(), "/install-packages.R")
 schedule <<- "DAILY"
 starttime <<- "23:00"
-
-
