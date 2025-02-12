81826da4ad2cc4d3cd3a1efdcca3217cc4e7e9fd
Foster <Tim.Foster@SEPA.org.uk>
2025-02-12 11:18:42 +0000

    update package install list

--- install-packages.R
+++ install-packages.R
@@ -13,22 +13,26 @@ install.packages(c(
   "stringr",
   "mgcv",
   "knitr",
-  "rmarkdown"
+  "rmarkdown",
+  "httr",
+  "ows4R"
 ),
 repos = "https://cloud.r-project.org"
 )
 
 # Install github packages ------------------------------------------------------
-devtools::install_github("ecodata1/darleq3",
-  ref = "description",
-  dependencies = TRUE,
-  upgrade = TRUE
-)
 devtools::install_github("ecodata1/hera",
   dependencies = TRUE,
   upgrade = TRUE
 )
-
+devtools::install_github("ecodata1/sepaStats",
+                         dependencies = TRUE,
+                         upgrade = TRUE
+)
+devtools::install_github("ecodata1/spotfireSepaStats",
+                         dependencies = TRUE,
+                         upgrade = TRUE
+)
 # Params for taskscheduler_create() function -----------------------------------
 # Create globals for set-up.R script
 taskname <<- "install_r_packages"
